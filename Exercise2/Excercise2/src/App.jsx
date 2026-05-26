d301b1ac6bad5ad8bd21585bb550b2403fb910ce
BROADSTREETBULLY <hjw.personal@gmail.com>
2026-05-26 17:53:13 +1000

    Excercise 2 completed

---
+++ Exercise2/Excercise2/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import BigCatsList from "./BigCats";
+import SingleCat from "./SingleCat";
+
+function App() {
+  
+  return (
+    <>
+    <div>
+      <BigCatsList></BigCatsList>
+      </div>
+      
+    </>
+  );
+}
+
+export default App;
